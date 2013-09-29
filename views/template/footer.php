a578c39145585d79b9fb0d2b4b48d3d5ee6ee7dd
Omar Diab <o.s.diab@gmail.com>
2013-09-29 13:41:17 -0700

    rmeoved php templates, replaced with new haml ones (navigation missing)

--- views/template/footer.php
+++
@@ -1,5 +0,0 @@
-<div id="footer">
-  <p>
-    <a href="/credits">Credits</a> | &copy; Omar Diab, 2013
-  </p>
-</div>
--- views/template/head.php
+++
@@ -1,5 +0,0 @@
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Omar Diab</title>
-  <link rel="stylesheet" href="/static/style/style.php" media="screen" />
-</head>
